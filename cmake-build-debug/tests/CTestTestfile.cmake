6d57063269db0046e176a47d9e0d6c847203cc85
xxmen <xxmen021@gmail.com>
2019-06-15 13:06:45 +0800

    init

--- cmake-build-debug/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/os/Desktop/TW/tests
-# Build directory: /Users/os/Desktop/TW/cmake-build-debug/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test "test_math.cpp")
-set_tests_properties(test PROPERTIES  _BACKTRACE_TRIPLES "/Users/os/Desktop/TW/tests/CMakeLists.txt;7;add_test;/Users/os/Desktop/TW/tests/CMakeLists.txt;0;")
